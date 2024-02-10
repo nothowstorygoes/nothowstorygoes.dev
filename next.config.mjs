ecab834d4b741f6bf5d79ac148eec8db0e3bb1e9
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-02-10 18:38:27 +0100

    Minor fixes

--- next.config.mjs
+++ next.config.mjs
@@ -13,8 +13,8 @@ const nextConfig = {
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
     basePath: "/nothowstorygoes.dev",
-    assetPrefix: "/nothowstorygoes.dev/"
-    
+    assetPrefix: "/nothowstorygoes.dev/",
+
     /**
      * Disable server-based image optimization. Next.js does not support
      * dynamic features with static exports.
