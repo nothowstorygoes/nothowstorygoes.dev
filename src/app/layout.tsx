6e52781510e956d49353a3f5d2041f06f8a243da
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-02-10 20:02:46 +0100

    Added meta description's content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="description" content="Pio Alessandro Esposito, BSc Computer Science student, Web Developer"/>
+      </head> 
       <body className={inter.className}>{children}</body>
     </html>
   );
