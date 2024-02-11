5970721f0ebd74b674075fd09380de7ac2d440a9
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-02-11 15:29:59 +0100

    Dropped slideshow component

--- src/app/prjarch/page.tsx
+++ src/app/prjarch/page.tsx
@@ -1,17 +1,15 @@
 "use client";
-import Slideshow from "../components/slideshow";
 import Sidebar from "../components/menuburger";
 import styles from "../css/prjarch.module.css";
 import useTypeWriter from "../components/typewriter";
 
 const Prj = () => {
-  const tw=useTypeWriter('This page is a collection of everything I have ever done.\n');
+  const tw=useTypeWriter('This page is a collection of \neverything I have ever done.\n');
   return (
     <div style={{ overflowY: "auto", height: "100vh" }} id="outer-container">
       <Sidebar />
       <main style={{ overflowY: "auto", height: "100vh" }} id="page-wrap" className={styles.main}>
           <pre className={styles.container}>{tw}</pre>
-        <Slideshow />
       </main>
     </div>
   );
