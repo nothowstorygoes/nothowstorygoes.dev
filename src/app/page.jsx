0061b7fc43d0425cc0d975b110ceafd809b87ac8
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-02-11 01:21:57 +0100

    Switched from CSS to react component for typewriter

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,9 +4,10 @@ import React from "react";
 import Sidebar from "./components/menuburger.js";
 import useTypeWriter from './components/typewriter';
 
-const tw = useTypeWriter('Hi There!\n \n This is my personal portfolio.\n Here you can find my\n project archive and all of my social platforms.');
+
 
 const Home = () => {
+  const tw = useTypeWriter('Hi There!\n \n This is my personal portfolio.\n Here you can find my\n project archive and all of my social platforms.');
   return (
     <div id="outer-container">
       <Sidebar />
