8702bac2f8a577bffcb710169d5c5730855d775a
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-02-11 01:34:26 +0100

    Style for TypeWriter

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,12 +7,14 @@ import useTypeWriter from './components/typewriter';
 
 
 const Home = () => {
-  const tw = useTypeWriter('Hi There!\n \n This is my personal portfolio.\n Here you can find my\n project archive and all of my social platforms.');
+  const tw = useTypeWriter('Hi There!\n \nThis is my personal portfolio.\nHere you can find my\nproject archive and all of my social platforms.');
   return (
     <div id="outer-container">
       <Sidebar />
       <main id="page-wrap">
+        <div className={styles.container}>
         <pre>{tw}</pre>
+        </div>
       </main>
     </div>
   );
