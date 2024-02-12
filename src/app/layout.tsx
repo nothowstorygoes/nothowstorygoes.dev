0151aae8fa02f6e24e96340399d1db93aa084880
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-02-12 23:14:09 +0100

    manifest edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" type="application/manifest+JSON" href="/nothowstorygoes.dev/manifest.webmanifest" />
-        <meta name="description" content="Pio Alessandro Esposito, BSc Computer Science student, Web Developer"/>
-      </head> 
+        <link rel="manifest" href="/nothowstorygoes.dev/manifest.json" />
+
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="application-name" content="nhsg.dev" />
+        <meta name="apple-mobile-web-app-title" content="nhsg.dev" />
+        <meta name="theme-color" content="#120c21" />
+        <meta name="msapplication-navbutton-color" content="#120c21" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="msapplication-starturl" content="/nothowstorygoes.dev/" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta
+          name="description"
+          content="Pio Alessandro Esposito, BSc Computer Science student, Web Developer"
+        />
+      </head>
       <body className={comfortaaa.className}>{children}</body>
     </html>
   );
