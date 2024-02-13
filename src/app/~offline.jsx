f2ed10c30621953399ee916901cf44611022456b
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-02-13 01:16:25 +0100

    Offline fallback

--- src/app/~offline.jsx
+++ src/app/~offline.jsx
@@ -1,10 +1,10 @@
 import useTypeWriter from './components/typewriter';
 import styles from './css/offline.module.css';
-
-const tw= useTypeWriter("Looks like you're offline.. ");
+import Image from 'next/image';
 
 export default function Offline()
 {
+    const tw= useTypeWriter("Looks like you're offline.. ");
     return(
         <main className={styles.offline}>
             <pre>{tw}</pre>
